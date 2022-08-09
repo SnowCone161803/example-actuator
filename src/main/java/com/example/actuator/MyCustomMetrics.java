6f2289ed1e46c9313fa0e0df1b8266ef607bf63d
Gordon Macrae <macraegordon@hotmail.com>
2022-08-09 18:48:23 +0100

    created custom metrics

---
+++ src/main/java/com/example/actuator/MyCustomMetrics.java
@@ -0,0 +1,32 @@
+package com.example.actuator;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class MyCustomMetrics {
+
+     private final MeterRegistry meterRegistry;
+     private final Flux<Long> everySecond = Flux.interval(Duration.ofSeconds(1));
+
+     @PostConstruct
+     public void init() {
+         // increment counter every second
+         everySecond.subscribe(this::incrementCounter);
+     }
+
+     public void incrementCounter(long value) {
+         meterRegistry.counter(
+             // this is the metrics/<endpoint> value
+             MyCustomMetrics.class.getSimpleName(),
+             // tags are optional
+             "typeOfTag:" + (value % 4), "tagValue:" + (value % 8)
+         ).increment();
+     }
+}
