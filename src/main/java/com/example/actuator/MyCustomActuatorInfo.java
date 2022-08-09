3ce49f95ac5edf4ffb9da9833f9ae9e0347f509f
Gordon Macrae <macraegordon@hotmail.com>
2022-08-09 18:17:45 +0100

    added custom actuator service

---
+++ src/main/java/com/example/actuator/MyCustomActuatorInfo.java
@@ -0,0 +1,23 @@
+package com.example.actuator;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class MyCustomActuatorInfo implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        final var items = Map.of(
+            "alpha", 100,
+            "beta", 200
+        );
+        builder.withDetail(
+            MyCustomActuatorInfo.class.getSimpleName(),
+            items
+        );
+    }
+}
