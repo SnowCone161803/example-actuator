e8d15e8071c0b2d292cd417d6cb14e5f74c34f56
Gordon Macrae <macraegordon@hotmail.com>
2022-08-09 19:08:46 +0100

    comment added for global metric tags

--- src/main/java/com/example/actuator/GlobalMetricConfiguration.java
+++ src/main/java/com/example/actuator/GlobalMetricConfiguration.java
@@ -10,7 +10,14 @@ public class GlobalMetricConfiguration {
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        // this tag gets added to all metric endpoints; why is this useful?!
+        // this tag gets added to all metric endpoints
+        // currently, I don't see why this is useful
+        // need to see how tags are being used in monitoring applications to understand what is going on here
+        //
+        // possibly good for:
+        // - microservice type
+        // - microservice instance ID
+        // it might make these things filterable in 3rd party tools
         return registry -> registry.config().commonTags("globalTagType", "globalTagValue");
     }
 }
